f89eb4398d9327df613ed2b489bc8d31f5acc9b9
Tim Otten <totten@civicrm.org>
2015-08-10 23:09:55 +0000

    JIRA - Put links in issue body.

--- scripts/hooks/jira.js
+++ scripts/hooks/jira.js
@@ -1,6 +1,7 @@
 var JiraChecker = require('../../lib/jira-checker.js');
 var CommentManager = require('../../lib/comment-manager.js');
 var _ = require('lodash');
+var locks = require('../../lib/lock.js');
 
 /**
  * Check the PR title and body for referneces to JIRA issues.
@@ -12,19 +13,29 @@ module.exports = function githubJiraHook(bot, repo_info, payload) {
   }
 
   var issueNumber = parseInt(payload.pull_request ? payload.pull_request.number : payload.issue.number);
-  var jiraChecker = new JiraChecker(bot.options.jira, payload.pull_request || payload.issue, repo_info);
+  var jiraChecker = new JiraChecker(bot.options.jira, payload.pull_request || payload.issue, repo_info, bot.github);
   var commentManager = CommentManager(bot.github, bot.options.username, repo_info);
 
+  // We're a bit promiscuous about accepting hooks. This is good for frequent updates
+  // but leads to some redundant notifications.
+  var lockName = 'jira:' + repo_info.full_name + ":" + issueNumber;
+  if (!locks.acquire(lockName)) return;
+
   console.log('[GithubJiraHook] Check for JIRA references');
-  var p = jiraChecker.check().then(function(msgs) {
-    var messages = {};
-    messages['civi-botdylan-jira'] = msgs;
-    console.log('[GithubJiraHook] Update comments', messages);
-    return commentManager.update(issueNumber, messages);
-  })
-  .catch(function(err){
-    bot.handleError(err);
-  });
+  var links = null;
+  var p = jiraChecker.getLinks()
+    .then(function(allLinks) {
+      links = allLinks;
+      console.log('[GithubJiraHook] Found links:', links);
+      return jiraChecker.addMissingLinks(links);
+    })
+    .then(function(){
+      return jiraChecker.updateLinksFromJira(links);
+    })
+    .finally(function(err){
+      locks.release(lockName);
+    })
+    .done();
 
   return bot.isSimulated ? p : null;
 };
